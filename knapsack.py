131f4ba645910ab5c9840030db232d77863de3b6
Vitor Diniz <vitordiniz25@gmail.com>
2021-09-05 13:18:43 -0300

    Adicionando knapsack

---
+++ knapsack.py
@@ -0,0 +1,23 @@
+def knapsack(v, w, C):
+    """
+    v: valores dos itens
+    w: peso dos itens
+    C: capacidade da mochila
+    """
+    N = len(v)
+    m = {}
+    for c in range(C+1):
+        m[(0,c)] = 0
+    for i in range(1, N+1):
+        for c in range(C+1):
+            if w[i-1] <= c:
+                m[(i,c)] = max(m[(i-1,c)], v[i-1] + m[(i-1,c-w[i-1])])
+            else: 
+                m[(i,c)] = m[(i-1,c)]
+    return m[(N,C)]
+
+v = [500, 250, 1500, 1200, 1200, 800]
+w = [4, 3, 10, 12, 9, 6]
+C = 30
+
+print(knapsack(v, w, C))
